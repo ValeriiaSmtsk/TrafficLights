d12dd793797f1e51dde5b18ce92799a96c66f67b
Salnik Valeriia <melfed.salnik@gmail.com>
2022-03-02 23:47:23 +0300

    Work with logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorOfLightsStackView: UIStackView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.layer.cornerRadius = 28
+        yellowLightView.layer.cornerRadius = 28
+        greenLightView.layer.cornerRadius = 28
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+               
     }
-
-
+    
+    
+    @IBAction func turnOnTrafficLightButton() {
+        
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("NEXT", for: .normal)
+            redLightView.alpha = 1
+        } else if startButton.currentTitle == "NEXT" {
+            if redLightView.alpha == 1 {
+                redLightView.alpha = 0.3
+                yellowLightView.alpha = 1
+            } else if yellowLightView.alpha == 1 {
+                yellowLightView.alpha = 0.3
+                greenLightView.alpha = 1
+            } else {
+                greenLightView.alpha = 0.3
+                redLightView.alpha = 1
+            }
+            
+        }
+        
+    }
 }
-
